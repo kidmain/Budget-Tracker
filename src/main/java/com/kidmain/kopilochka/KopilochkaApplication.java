ec6ae20e2884f253901d3051109905fb46c6d6eb
Kidmain <thekidmain@gmail.com>
2022-12-07 13:25:52 +0300

    Initial commit

---
+++ src/main/java/com/kidmain/kopilochka/KopilochkaApplication.java
@@ -0,0 +1,13 @@
+package com.kidmain.kopilochka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KopilochkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KopilochkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kidmain/kopilochka/KopilochkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kidmain.kopilochka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KopilochkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
