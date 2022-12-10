45aec8a08d911b193a0dd49ce478d47b620f5acb
Kidmain <thekidmain@gmail.com>
2022-12-10 11:04:49 +0300

    Add JDBC template configuration

--- src/main/java/com/kidmain/kopilochka/configs/KopilochkaConfig.java
+++ src/main/java/com/kidmain/kopilochka/configs/KopilochkaConfig.java
@@ -1,9 +1,45 @@
 package com.kidmain.kopilochka.configs;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.util.Objects;
 
 @Configuration
 @ComponentScan(basePackages = "com.kidmain.kopilochka")
+@PropertySource("classpath:application.yml")
 public class KopilochkaConfig {
+
+    /**
+     * JDBC Template
+     */
+    private final Environment environment;
+
+    @Autowired
+    public KopilochkaConfig(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(Objects.requireNonNull(environment.getProperty("spring.datasource.driver-class-name")));
+        dataSource.setUrl(environment.getProperty("spring.datasource.url"));
+        dataSource.setSchema(environment.getProperty("spring.jpa.properties.hibernate.default_schema"));
+        dataSource.setUsername(environment.getProperty("spring.datasource.username"));
+        dataSource.setPassword(environment.getProperty("spring.datasource.password"));
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
+    }
 }
