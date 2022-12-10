ec2080941a39624a3e5628927fd4d845f8cc08b2
Kidmain <thekidmain@gmail.com>
2022-12-10 09:28:58 +0300

    Set up spring application

--- src/main/java/com/kidmain/kopilochka/KopilochkaApplication.java
+++ src/main/java/com/kidmain/kopilochka/KopilochkaApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KopilochkaApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(KopilochkaApplication.class, args);
     }
-
 }
