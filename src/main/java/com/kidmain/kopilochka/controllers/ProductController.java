79df9f8c2ee56fbf398786504b4b6a162f2a45e4
Kidmain <thekidmain@gmail.com>
2022-12-10 11:06:35 +0300

    Use JDBC Template and handle invalid inputs

--- src/main/java/com/kidmain/kopilochka/controllers/ProductController.java
+++ src/main/java/com/kidmain/kopilochka/controllers/ProductController.java
@@ -2,23 +2,27 @@ package com.kidmain.kopilochka.controllers;
 
 import com.kidmain.kopilochka.models.Product;
 import com.kidmain.kopilochka.services.ProductService;
+import com.kidmain.kopilochka.services.ProductServiceTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/products")
 public class ProductController {
-    private final ProductService productService;
+    private final ProductServiceTemplate productService;
 
-    @Autowired
-    public ProductController(ProductService productService) {
+    public ProductController(ProductServiceTemplate productService) {
         this.productService = productService;
     }
 
@@ -36,12 +40,19 @@ public class ProductController {
     }
 
     @PostMapping
-    public String addProduct(@ModelAttribute("product") Product product) {
+    public String addProduct(
+            @ModelAttribute("product")
+            @Valid Product product, BindingResult bindingResult) {
+
+        if(bindingResult.hasErrors()) {
+            return "products/products";
+        }
+
         productService.addProduct(product);
         return "redirect:/products";
     }
 
-    @PostMapping("/{id}/delete")
+    @DeleteMapping("/{id}/delete")
     public String deleteProduct(@PathVariable("id") Long id) {
         productService.deleteProductById(id);
         return "redirect:/products";
@@ -54,11 +65,15 @@ public class ProductController {
         return "products/products-edit";
     }
 
-    @PutMapping("/{id}/edit")
+    @PatchMapping("/{id}/edit")
     public String editProduct(
             @PathVariable("id") Long id,
-            Product product
+            @Valid Product product, BindingResult bindingResult
     ) {
+        if(bindingResult.hasErrors()) {
+            return "products/products-edit";
+        }
+
         Product updatedProduct = productService.getProductById(id);
 
         if (product.getName() != null) updatedProduct.setName(product.getName());
