968b5afe9f70fc2dee74a20404b63b06172c3418
Kidmain <thekidmain@gmail.com>
2022-12-10 09:29:54 +0300

    Add configuration class

---
+++ src/main/java/com/kidmain/kopilochka/configs/KopilochkaConfig.java
@@ -0,0 +1,9 @@
+package com.kidmain.kopilochka.configs;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.kidmain.kopilochka")
+public class KopilochkaConfig {
+}
