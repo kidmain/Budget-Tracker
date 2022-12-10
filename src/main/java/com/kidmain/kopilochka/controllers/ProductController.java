4f28ad97814a39fd413a06260142a569f06308e8
Kidmain <thekidmain@gmail.com>
2022-12-10 09:33:53 +0300

    Product controller

---
+++ src/main/java/com/kidmain/kopilochka/controllers/ProductController.java
@@ -0,0 +1,72 @@
+package com.kidmain.kopilochka.controllers;
+
+import com.kidmain.kopilochka.models.Product;
+import com.kidmain.kopilochka.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/products")
+public class ProductController {
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public String getAllProducts(Model model) {
+        model.addAttribute("products", productService.getAllProductsByOrder());
+        model.addAttribute("product", new Product());
+        return "products/products";
+    }
+
+    @GetMapping("/{id}")
+    public String getProductById(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("product", productService.getProductById(id));
+        return "products/product";
+    }
+
+    @PostMapping
+    public String addProduct(@ModelAttribute("product") Product product) {
+        productService.addProduct(product);
+        return "redirect:/products";
+    }
+
+    @PostMapping("/{id}/delete")
+    public String deleteProduct(@PathVariable("id") Long id) {
+        productService.deleteProductById(id);
+        return "redirect:/products";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String getEditProductById(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("product", productService.getProductById(id));
+        model.addAttribute("products", productService.getAllProducts());
+        return "products/products-edit";
+    }
+
+    @PutMapping("/{id}/edit")
+    public String editProduct(
+            @PathVariable("id") Long id,
+            Product product
+    ) {
+        Product updatedProduct = productService.getProductById(id);
+
+        if (product.getName() != null) updatedProduct.setName(product.getName());
+        if (product.getPrice() != null) updatedProduct.setPrice(product.getPrice());
+        if (product.getDate() != null) updatedProduct.setDate(product.getDate());
+        if (product.getUsername() != null) updatedProduct.setUsername(product.getUsername());
+
+        productService.updateProduct(updatedProduct);
+        return "redirect:/products";
+    }
+}
