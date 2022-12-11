09ddcda58dc933e01fb136aac63a1f60baf4e4d9
Kidmain <thekidmain@gmail.com>
2022-12-11 15:01:17 +0300

    Change location of files + add User edit

---
+++ src/main/java/com/kidmain/kopilochka/controllers/IndexController.java
@@ -0,0 +1,31 @@
+package com.kidmain.kopilochka.controllers;
+
+import com.kidmain.kopilochka.models.AppUser;
+import com.kidmain.kopilochka.models.Product;
+import com.kidmain.kopilochka.services.ProductService;
+import com.kidmain.kopilochka.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    private final ProductService productService;
+    private final UserService userService;
+
+    @Autowired
+    public IndexController(ProductService productService, UserService userService) {
+        this.productService = productService;
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public String getAllProducts(Model model) {
+        model.addAttribute("product", new Product());
+        model.addAttribute("products", productService.getAllProductsByOrder());
+        model.addAttribute("user", new AppUser());
+        model.addAttribute("users", userService.getAllUsersByOrder());
+        return "index";
+    }
+}
