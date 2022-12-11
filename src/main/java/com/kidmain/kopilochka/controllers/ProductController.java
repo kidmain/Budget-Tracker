76cf33e80139c29777ad4a069e4385fc5bf6c8bb
Kidmain <thekidmain@gmail.com>
2022-12-11 09:00:21 +0300

    add user

--- src/main/java/com/kidmain/kopilochka/controllers/ProductController.java
+++ src/main/java/com/kidmain/kopilochka/controllers/ProductController.java
@@ -1,33 +1,37 @@
 package com.kidmain.kopilochka.controllers;
 
+import com.kidmain.kopilochka.models.AppUser;
 import com.kidmain.kopilochka.models.Product;
-import com.kidmain.kopilochka.services.ProductServiceTemplate;
+import com.kidmain.kopilochka.services.ProductService;
+import com.kidmain.kopilochka.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.validation.Valid;
-
 @Controller
 @RequestMapping("/products")
 public class ProductController {
-    private final ProductServiceTemplate productService;
+    private final ProductService productService;
+    private final UserService userService;
 
-    public ProductController(ProductServiceTemplate productService) {
+    @Autowired
+    public ProductController(ProductService productService, UserService userService) {
         this.productService = productService;
+        this.userService = userService;
     }
 
     @GetMapping
     public String getAllProducts(Model model) {
-        model.addAttribute("products", productService.getAllProductsByOrder());
         model.addAttribute("product", new Product());
+        model.addAttribute("products", productService.getAllProductsByOrder());
+        model.addAttribute("user", new AppUser());
+        model.addAttribute("users", userService.getAllUsers());
         return "products/products";
     }
 
@@ -38,14 +42,8 @@ public class ProductController {
     }
 
     @PostMapping
-    public String addProduct(
-            @ModelAttribute("product")
-            @Valid Product product, BindingResult bindingResult) {
-
-        if(bindingResult.hasErrors()) {
-            return "products/products";
-        }
-
+    public String addProduct(Product product, AppUser user) {
+        product.setUser(user);
         productService.addProduct(product);
         return "redirect:/products";
     }
@@ -66,24 +64,23 @@ public class ProductController {
     public String getEditProductById(@PathVariable("id") Long id, Model model) {
         model.addAttribute("product", productService.getProductById(id));
         model.addAttribute("products", productService.getAllProducts());
+        model.addAttribute("user", productService.getProductById(id).getUser());
+        model.addAttribute("users", userService.getAllUsers());
         return "products/products-edit";
     }
 
     @PatchMapping("/{id}/edit")
-    public String editProduct(
-            @PathVariable("id") Long id,
-            @Valid Product product, BindingResult bindingResult
-    ) {
-        if(bindingResult.hasErrors()) {
-            return "products/products-edit";
-        }
+    public String editProduct(@PathVariable("id") Long id, Product product, AppUser user) {
+//        if(bindingResult.hasErrors()) {
+//            return "products/products-edit";
+//        }
 
         Product updatedProduct = productService.getProductById(id);
 
         if (product.getName() != null) updatedProduct.setName(product.getName());
         if (product.getPrice() != null) updatedProduct.setPrice(product.getPrice());
         if (product.getDate() != null) updatedProduct.setDate(product.getDate());
-        if (product.getUsername() != null) updatedProduct.setUsername(product.getUsername());
+        updatedProduct.setUser(user);
 
         productService.updateProduct(updatedProduct);
         return "redirect:/products";
