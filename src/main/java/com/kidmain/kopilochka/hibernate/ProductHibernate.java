8e7f07b6351bbe38d3770b92c03adbedddd8eaaa
Kidmain <thekidmain@gmail.com>
2022-12-11 09:01:01 +0300

    HQL + method to add random rows

--- src/main/java/com/kidmain/kopilochka/hibernate/ProductHibernate.java
+++ src/main/java/com/kidmain/kopilochka/hibernate/ProductHibernate.java
@@ -1,5 +1,6 @@
 package com.kidmain.kopilochka.hibernate;
 
+import com.kidmain.kopilochka.models.AppUser;
 import com.kidmain.kopilochka.models.Product;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -10,19 +11,28 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * ////////////////////////////////////////////
+ * USELESS GARBAGE CLASS FOR LEARNING HIBERNATE
+ * ////////////////////////////////////////////
+ */
+
 @Service
 public class ProductHibernate {
     // hibernate.properties
-    private static final Configuration configuration = new Configuration().addAnnotatedClass(Product.class);
+    private static final Configuration configuration = new Configuration()
+            .addAnnotatedClass(Product.class)
+            .addAnnotatedClass(AppUser.class);
 
     public static void main(String[] args) {
-        ProductHibernate hibernate = new ProductHibernate();
-//        hibernate.addRandomProducts();
-//        hibernate.editProductName(2L, "John");
-        hibernate.deleteProduct(2L);
+//        addProduct();
+        addRandomProducts();
+//        editProductName(1L, "UPDATED");
+//        deleteProduct(2L);
+//        HQL(false);
     }
 
-    private Product getProductById(Long id) {
+    private static Product getProductById(Long id) {
         SessionFactory sessionFactory = configuration.buildSessionFactory();
 
         try (sessionFactory) {
@@ -36,24 +46,43 @@ public class ProductHibernate {
         }
     }
 
-    private void addRandomProducts() {
-        ProductHibernate hibernate = new ProductHibernate();
+    private static void addProduct() {
         SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.getCurrentSession();
 
-        List<Product> products = new ArrayList<>();
-        for (int i = 0; i < 15; i++) {
-            products.add(
-                    new Product(
-                            hibernate.createRandomWord((int) (Math.random() * 50) + 1),
-                            Math.random() * 10000,
-                            LocalDate.of((int) (Math.random() * 2022), (int) (Math.random() * 11 + 1), (int) (Math.random() * 27 + 1)),
-                            "Anna")
-            );
+        try (sessionFactory) {
+            session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
+
+            AppUser user = session.get(AppUser.class, 1L);
+            Product product = new Product(
+                    createRandomWord((int) (Math.random() * 50) + 1),
+                    Math.random() * 10000,
+                    LocalDate.of((int) (Math.random() * 2022), (int) (Math.random() * 11 + 1), (int) (Math.random() * 27 + 1)),
+                    user);
+
+            session.save(product);
+            session.getTransaction().commit();
         }
+    }
+
+    private static void addRandomProducts() {
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
 
         try {
             session.beginTransaction();
+
+            List<Product> products = new ArrayList<>();
+            for (int i = 0; i < 15; i++) {
+                products.add(
+                        new Product(
+                                createRandomWord((int) (Math.random() * 50) + 1),
+                                Double.parseDouble(String.format("%.2f", (Math.random() * 10000 + 1))),
+                                LocalDate.of((int) (Math.random() * 2022), (int) (Math.random() * 12 + 1), (int) (Math.random() * 28 + 1)),
+                                session.get(AppUser.class, (long) (Math.random() * 2) + 1))
+                );
+            }
 
             for (Product product : products) {
                 session.save(product);
@@ -66,7 +95,7 @@ public class ProductHibernate {
         }
     }
 
-    private String createRandomWord(int len) {
+    private static String createRandomWord(int len) {
         StringBuilder word = new StringBuilder();
         for (int i = 0; i < len; i++) {
             int v = 1 + (int) (Math.random() * 26);
@@ -76,7 +105,7 @@ public class ProductHibernate {
         return word.toString();
     }
 
-    private void editProductName(Long id, String name) {
+    private static void editProductName(Long id, String name) {
         SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.getCurrentSession();
 
@@ -93,7 +122,7 @@ public class ProductHibernate {
         }
     }
 
-    private void deleteProduct(Long id) {
+    private static void deleteProduct(Long id) {
         SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.getCurrentSession();
 
@@ -104,6 +133,34 @@ public class ProductHibernate {
             session.delete(product);
 
             session.getTransaction().commit();
+        } finally {
+            session.close();
+            sessionFactory.close();
+        }
+    }
+
+    private static void HQL(boolean isUpdate) {
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+
+        try {
+            session.beginTransaction();
+
+            List<Product> products = new ArrayList<>();
+            session
+                    .createQuery("FROM Product WHERE user.id = 1 ")
+                    .getResultStream()
+                    .forEach( product -> products.add((Product) product) );
+
+            if (isUpdate) {
+                session
+                        .createQuery("UPDATE Product SET name='UPDATED'")
+                        .executeUpdate();
+            }
+
+            session.getTransaction().commit();
+
+            products.forEach(System.out::println);
         } finally {
             session.close();
             sessionFactory.close();
