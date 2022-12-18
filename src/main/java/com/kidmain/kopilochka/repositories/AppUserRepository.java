f339b41c0c79ff3bad93ac722bc362768a6b18f1
Kidmain <thekidmain@gmail.com>
2022-12-18 09:37:06 +0300

    minor changes

--- src/main/java/com/kidmain/kopilochka/repositories/AppUserRepository.java
+++ src/main/java/com/kidmain/kopilochka/repositories/AppUserRepository.java
@@ -3,12 +3,10 @@ package com.kidmain.kopilochka.repositories;
 import com.kidmain.kopilochka.models.AppUser;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
-//@Transactional(readOnly = true)
-public interface UserRepository extends CrudRepository<AppUser, Long> {
+public interface AppUserRepository extends CrudRepository<AppUser, Long> {
     List<AppUser> findByOrderById();
 }
--- src/main/java/com/kidmain/kopilochka/repositories/ProductRepository.java
+++ src/main/java/com/kidmain/kopilochka/repositories/ProductRepository.java
@@ -3,13 +3,11 @@ package com.kidmain.kopilochka.repositories;
 import com.kidmain.kopilochka.models.Product;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-//@Transactional(readOnly = true)
 public interface ProductRepository extends CrudRepository<Product, Long> {
     List<Product> findByOrderById();
     Optional<List<Product>> findByUserId(Long id);
